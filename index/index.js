b2fd813af681c0f851c967b38f1acd19ad79a742
matheus <math99@outlook.com>
2022-05-27 20:12:59 -0300

    API integrada e funcionando corretamente

--- index/index.js
+++ index/index.js
@@ -1,27 +1,41 @@
 var date;
 
 var requisicao = new XMLHttpRequest();
-var responseOBJ ;
-
-let button = document.querySelector("button")
+var responseOBJ;
 
 
-
-
-button.addEventListener("click", function () {
+function request() {
 
 
     date = document.querySelector("input").value
 
     requisicao.open("GET", `https://api.nasa.gov/planetary/apod?api_key=6nCIEbLbYtiE1jUFigUHZ8ACvffqPRfZrdR0Qzvs&date=${date}`)
+
     requisicao.send();
-})
+}
 
 requisicao.onload = function () {
 
+    const picture = document.querySelector("#img")
+    const text = document.querySelector("#texto")
+console.log(picture)
+console.log(text)
+
+
     if (requisicao.status == 200) {
-        let response = requisicao.response;
+        const response = requisicao.response;
         responseOBJ = JSON.parse(response);
-        console.log(responseOBJ);
+
+        const texto = responseOBJ.explanation
+        const img = responseOBJ.url
+
+        picture.setAttribute("src",img)
+
+        text.innerHTML = texto
+
+        console.log(texto);
+        console.log(img);
+
+
     }
 }
